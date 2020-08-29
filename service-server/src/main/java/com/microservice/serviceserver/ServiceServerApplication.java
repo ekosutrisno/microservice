695fb41ae3e54ffa4ab4f4a70fd5014d0baa6607
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2020-08-29 12:11:56 +0700

    cloud: Add Eureka Client and EnableDiscoveryClient

--- service-server/src/main/java/com/microservice/serviceserver/ServiceServerApplication.java
+++ service-server/src/main/java/com/microservice/serviceserver/ServiceServerApplication.java
@@ -2,6 +2,7 @@ package com.microservice.serviceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.security.Principal;
 
 @SpringBootApplication
 @EnableResourceServer
+@EnableDiscoveryClient
 @RestController
 public class ServiceServerApplication {
    @RequestMapping("/user")
