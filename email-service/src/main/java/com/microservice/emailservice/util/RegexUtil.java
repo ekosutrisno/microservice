cf3b174a4acc1e7b8d2650e8372f968998c9555e
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2020-09-11 19:46:39 +0700

    Add Email Service for Handling Messaging and Info

---
+++ email-service/src/main/java/com/microservice/emailservice/util/RegexUtil.java
@@ -0,0 +1,55 @@
+package com.microservice.emailservice.util;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * RegexUtil
+ */
+public class RegexUtil {
+
+  public static boolean validateEmail(String email) {
+
+    String EMAIL_REGEX = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+    Pattern pattern = Pattern.compile(EMAIL_REGEX, Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher(email);
+    return matcher.matches();
+  }
+
+  public static boolean validUsername(String username) {
+    String USERNAME_REGEX = "[A-Za-z0-9_]+";
+    return username.matches(USERNAME_REGEX);
+  }
+
+  public static boolean validatePassword(String password, Map<String, String> maps) {
+    Pattern spesialCharPattern = Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
+    Pattern upperCasePattern = Pattern.compile("[A-Z ]");
+    Pattern lowerCasePattern = Pattern.compile("[a-z ]");
+    Pattern digitalCasePattern = Pattern.compile("[0-9 ]");
+
+    boolean kondisi = true;
+
+    if (password.isEmpty()) {
+      maps.put("errorMessage", "Password tidak boleh kosong");
+      kondisi = false;
+    } else if (password.length() < 6) {
+      maps.put("errorMessage", "Password minimal 6 karakter");
+      kondisi = false;
+    } else if (!spesialCharPattern.matcher(password).find()) {
+      maps.put("errorMessage", "Password minimal terdapat 1 karakter spesial karakter");
+      kondisi = false;
+    } else if (!upperCasePattern.matcher(password).find()) {
+      maps.put("errorMessage", "Password minimal terdapat 1 karakter Hurup Kapital");
+      kondisi = false;
+    } else if (!lowerCasePattern.matcher(password).find()) {
+      maps.put("errorMessage", "Password minimal terdapat 1 karakter hurup kecil");
+      kondisi = false;
+    } else if (!digitalCasePattern.matcher(password).find()) {
+      maps.put("errorMessage", "Password minimal terdapat 1 karakter angka");
+      kondisi = false;
+    }
+    return kondisi;
+  }
+
+}
